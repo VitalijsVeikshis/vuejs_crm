e43ed030cd0eb6b40e2c209d511616a160486faf
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-09-11 14:48:39 +0300

    test: replace POST request in receive_token_credentials by create_new_auth_token call

--- spec/support/helpers/request_helpers.rb
+++ spec/support/helpers/request_helpers.rb
@@ -3,10 +3,7 @@
 module Helpers
   module RequestHelpers
     def receive_token_credentials(user)
-      post "/#{user.class.name.downcase}/auth/sign_in/",
-           params: { email: user.email, password: user.password }
-
-      response.headers.slice('client', 'access-token', 'uid')
+      user.create_new_auth_token.slice('client', 'access-token', 'uid')
     end
   end
 end
