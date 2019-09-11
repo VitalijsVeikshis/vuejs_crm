08a11cd53f9d7d2c07055ed5dce57e1d24e09677
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-09-11 10:44:56 +0300

    test: delete shared examples api_authentication

--- spec/controllers/client/landing_controller_spec.rb
+++ spec/controllers/client/landing_controller_spec.rb
@@ -6,55 +6,30 @@ RSpec.describe Client::LandingController, type: :controller do
   describe 'GET #index' do
     context 'when user is client' do
       let(:client) { create(:client) }
+      let(:request_params) { { method: :get, action: :index } }
 
-      context 'when cookie authentication' do
-        let(:request_params) { { method: :get, action: :index } }
+      before { sign_in_as(client) }
 
-        before { sign_in_as(client) }
+      it 'renders index template' do
+        do_request(request_params)
 
-        it 'renders index template' do
-          do_request(request_params)
-
-          expect(response).to render_template :index
-        end
-
-        it 'renders client layout' do
-          do_request(request_params)
-
-          expect(response).to render_template(layout: :client)
-        end
+        expect(response).to render_template :index
       end
 
-      context 'when token authentication' do
-        let(:options) do
-          { client_email: client.email, client_token: client.authentication_token,
-            format: :json }
-        end
-        let(:request_params) { { method: :get, action: :index, options: options } }
+      it 'renders client layout' do
+        do_request(request_params)
 
-        it_behaves_like 'API success request', :ok
+        expect(response).to render_template(layout: :client)
       end
     end
 
     context 'when user is not client' do
       let(:request_params) { { method: :get, action: :index } }
 
-      context 'when cookie authentication' do
-        it 'redirects to root' do
-          do_request(request_params)
+      it 'redirects to root' do
+        do_request(request_params)
 
-          expect(response).to redirect_to root_path
-        end
-      end
-
-      context 'when token authentication' do
-        let(:options) do
-          { client_email: 'wrong@test.email', client_token: 'wrong_token',
-            format: :json }
-        end
-        let(:request_params) { { method: :get, action: :index, options: options } }
-
-        it_behaves_like 'API failure request', :unauthorized
+        expect(response).to redirect_to root_path
       end
     end
   end
--- spec/controllers/staff/landing_controller_spec.rb
+++ spec/controllers/staff/landing_controller_spec.rb
@@ -6,55 +6,30 @@ RSpec.describe Staff::LandingController, type: :controller do
   describe 'GET #index' do
     context 'when user is staff' do
       let(:staff) { create(:staff) }
+      let(:request_params) { { method: :get, action: :index } }
 
-      context 'when cookie authentication' do
-        let(:request_params) { { method: :get, action: :index } }
+      before { sign_in_as(staff) }
 
-        before { sign_in_as(staff) }
+      it 'renders index template' do
+        do_request(request_params)
 
-        it 'renders index template' do
-          do_request(request_params)
-
-          expect(response).to render_template :index
-        end
-
-        it 'renders staff layout' do
-          do_request(request_params)
-
-          expect(response).to render_template(layout: :staff)
-        end
+        expect(response).to render_template :index
       end
 
-      context 'when token authentication' do
-        let(:options) do
-          { staff_email: staff.email, staff_token: staff.authentication_token,
-            format: :json }
-        end
-        let(:request_params) { { method: :get, action: :index, options: options } }
+      it 'renders staff layout' do
+        do_request(request_params)
 
-        it_behaves_like 'API success request', :ok
+        expect(response).to render_template(layout: :staff)
       end
     end
 
     context 'when user is not staff' do
       let(:request_params) { { method: :get, action: :index } }
 
-      context 'when cookie authentication' do
-        it 'redirects to root' do
-          do_request(request_params)
+      it 'redirects to root' do
+        do_request(request_params)
 
-          expect(response).to redirect_to root_path
-        end
-      end
-
-      context 'when token authentication' do
-        let(:options) do
-          { staff_email: 'wrong@test.email', staff_token: 'wrong_token',
-            format: :json }
-        end
-        let(:request_params) { { method: :get, action: :index, options: options } }
-
-        it_behaves_like 'API failure request', :unauthorized
+        expect(response).to redirect_to root_path
       end
     end
   end
