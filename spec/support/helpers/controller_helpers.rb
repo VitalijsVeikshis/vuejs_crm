abac3a0e70f860e55730753ee91a5a66a80e1d6b
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2019-09-19 20:29:16 +0300

    test: add format to response_json helper

--- spec/support/helpers/controller_helpers.rb
+++ spec/support/helpers/controller_helpers.rb
@@ -12,7 +12,8 @@ module Helpers
     def do_request(request_params)
       send request_params.dig(:method),
            request_params.dig(:action),
-           params: request_params.dig(:options)
+           params: request_params.dig(:options),
+           format: request_params.dig(:format)
     end
 
     def response_json
